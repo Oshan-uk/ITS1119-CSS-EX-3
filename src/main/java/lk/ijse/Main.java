80474d1d1c2e041b5a3e8dcc0b9c360ee057540f
Oshan <git config --global user.email>
2026-03-05 08:50:56 +0530

    update the project

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -1,4 +1,4 @@
-package lk.ijse.mlsupermarket;
+package lk.ijse;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
